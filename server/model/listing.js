c840db9d13880a64b6c45418474b95c496cf16e4
Mohammed Tabish Khan <161915192+Tabishkh07@users.noreply.github.com>
2026-01-05 22:06:09 +0530

    feat: fixed the image default link.

--- server/model/listing.js
+++ server/model/listing.js
@@ -5,13 +5,17 @@ const listingSchema = new Schema({
     title: {
         type: String,
         required: true,
-    },
+    },   
     description: String,
-    image:{
-        default:"https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjAЗfDB8MHxzZWFyY2h8MTJ8fGdvYXxIbnww",
-        type: String,
-        set:(v)=> v===''?"https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjAЗfDB8MHxzZWFyY2h8MTJ8fGdvYXxIbnww"
-        :v,
+    image: {
+        url: {
+            type: String,
+            default: "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?auto=format&fit=crop&w=800&q=60"
+        },
+    filename: {
+            type: String,
+            default: "default"
+        }
     },
     price: Number,
     location: String,
