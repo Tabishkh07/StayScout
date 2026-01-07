bb28af89324cdc6c8d22224738d5808edcd6e069
Mohammed Tabish Khan <161915192+Tabishkh07@users.noreply.github.com>
2026-01-07 22:22:33 +0530

    feat: Handled Server side errors using joi

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
+const {listingSchema} = require("./schema.js")
 
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
@@ -42,8 +43,20 @@ app.get("/", (req, res)=>{
     res.send("hi am root");
 });
 
+// middleware for joi error handling
+const validateListing = (req, res, next)=>{
+    let {error} = listingSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    }else{
+        next();
+    }
+};
+
 // index route
-app.get("/listings", wrapAsync(async(req, res)=>{
+app.get("/listings", 
+    wrapAsync(async(req, res)=>{
     const allListing = await Listing.find({});
     res.render("listings/index.ejs", {allListing});
 }));
@@ -61,10 +74,12 @@ app.get("/listings/:id", wrapAsync(async(req, res)=>{
 }));
 
 // create route
-app.post("/listings", wrapAsync(async(req, res, next)=>{
+app.post("/listings",
+    validateListing,
+    wrapAsync(async(req, res, next)=>{
     const newListing = new Listing(req.body.listing);
     await newListing.save();
-    res.redirect("/listings")
+    res.redirect("/listings");
 }));
 
 // edit route
@@ -75,7 +90,9 @@ app.get("/listings/:id/edit", wrapAsync(async(req, res)=>{
 }));
 
 // update route
-app.put("/listings/:id", wrapAsync(async(req, res)=>{
+app.put("/listings/:id",
+    validateListing, 
+    wrapAsync(async(req, res)=>{
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});
     res.redirect(`/listings/${id}`);
@@ -96,7 +113,8 @@ app.use((req, res, next)=>{
 
 app.use((err, req, res, next)=>{
     let {statusCode=500, message = "Something went wrong!!"} = err;
-    res.status(statusCode).send(message);
+    res.status(statusCode).render("error.ejs", {message});
+    // res.status(statusCode).send(message);
 });
 
 let port = 8080;
