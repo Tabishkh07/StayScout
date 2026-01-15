55ef548ff40385fba288cf09ad49d16030f5eab9
Mohammed Tabish Khan <161915192+Tabishkh07@users.noreply.github.com>
2026-01-15 22:52:33 +0530

    Added: Review route.

--- server/app.js
+++ server/app.js
@@ -7,7 +7,8 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema} = require("./schema.js")
+const {listingSchema} = require("./schema.js");
+const Review = require("./models/review.js");
 
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
@@ -105,6 +106,19 @@ app.delete("/listings/:id", wrapAsync(async(req, res)=>{
     console.log (deletedListing);
     res.redirect("/listings");
 }));
+
+// REVIEW
+// post route
+app.post("/listings/:id/reviews", async(req, res)=>{
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+
+    listing. reviews.push (newReview);
+
+    await newReview.save();
+    await listing. save();
+    res.redirect(`listings/${listing._id}`);
+});
 
 // for url which dosent exist
 app.use((req, res, next)=>{
