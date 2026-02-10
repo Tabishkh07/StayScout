96b0fd8e0aed7c6501b2526b3abf3f31bb4a413f
Mohammed Tabish Khan <161915192+Tabishkh07@users.noreply.github.com>
2026-02-10 06:48:40 +0530

    using router.route for compact code

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,39 +1,28 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user.js");
-const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
+const {saveRedirectUrl} = require("../middleware.js");
 
-// for signup form
-router.get("/signup", (req, res)=>{
-    res.render("users/signup.ejs");
-});
+const userController = require("../controllers/users.js");
 
-// for saving in signup data in database
-router.post("/signup", wrapAsync(async(req,res)=>{
-    try{
-        let {username, email, password} = req.body;
-    const newUser = new User({email, username});
-    const registerdUser = await User.register(newUser, password);
-    console.log(registerdUser);
-    req.flash("success", "Welcome to StayScout");
-    res.redirect("/listings");
-    }catch(err){
-        req.flash("error", err.message);
-        res.redirect("/signup");
-    }
-}));
+// compact way
+router
+    .route("/signup")
+    .get(userController.renderSignupForm)   // for signup form
+    .post(userController.signup);    // for saving in signup data in database
 
-// for login
-router.get("/login", (req, res)=>{
-    res.render("users/login.ejs");
-});
+router
+    .route("/login")
+    .get(userController.reanderLoginForm)    // for login
+    .post(saveRedirectUrl,    // to check if user exits
+    passport.authenticate("local", 
+        {failureRedirect: '/login', 
+            failureFlash: true
+        }),
+    userController.login
+);
 
-// to check if user exits
-router.post("/login", passport.authenticate("local", {failureRedirect: '/login', failureFlash: true}),
-    async(req, res)=>{
-        req. flash("success", "Welcome back to StayScout!!");
-        res. redirect("/listings");
-    });
+// logout route
+router.get("/logout", userController.logout);
 
 module.exports = router;
