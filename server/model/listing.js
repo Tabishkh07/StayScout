b5aff6282db72134d44396c9f8d9eb442d66d810
Mohammed Tabish Khan <161915192+Tabishkh07@users.noreply.github.com>
2026-02-11 21:55:33 +0530

    feat: edited the listing image schema

--- server/model/listing.js
+++ server/model/listing.js
@@ -9,14 +9,8 @@ const listingSchema = new Schema({
     },   
     description: String,
     image: {
-        url: {
-            type: String,
-            default: "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?auto=format&fit=crop&w=800&q=60"
-        },
-    filename: {
-            type: String,
-            default: "default"
-        }
+        url: String,
+        filename: String,
     },
     price: Number,
     location: String,
