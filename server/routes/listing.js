ca4d2345b8e1f4122512a606e00e3265e3013939
Mohammed Tabish Khan <161915192+Tabishkh07@users.noreply.github.com>
2026-02-11 21:57:55 +0530

    feat: added the multer and cloudconfig

--- server/routes/listing.js
+++ server/routes/listing.js
@@ -2,14 +2,21 @@ const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const {isLoggedIn, isOwner, validateListing} = require("../middleware.js");
-
 const listingController = require("../controllers/listings.js");
+const multer = require ('multer');
+const {storage} = require("../cloudConfig.js");
+const upload = multer ({storage});
 
 // compact way of writing same routes
 router
     .route("/")
-    .get(wrapAsync(listingController.index))   // index route
-    .post(isLoggedIn, validateListing, wrapAsync(listingController.createListing));  //create route
+    .get (wrapAsync (listingController. index))
+    .post(
+        isLoggedIn,
+        upload.single("listing[image]"),
+        validateListing,
+        wrapAsync(listingController.createListing)
+    );
     
 // new route
 router.get("/new", isLoggedIn, listingController.renderNewForm);    // if below id then confusion in id and new
@@ -17,10 +24,18 @@ router.get("/new", isLoggedIn, listingController.renderNewForm);    // if below 
 router
     .route("/:id")
     .get(wrapAsync(listingController.showListing)) // show route
-    .put(isLoggedIn, validateListing, isOwner, wrapAsync(listingController.updateListing)) // update route
+    .put(isLoggedIn,
+        isOwner, 
+        upload.single("listing[image]"),
+        validateListing,  
+        wrapAsync(listingController.updateListing)) // update route
     .delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));  // delete route
 
 // edit route
-router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(listingController.renderEditForm));
+router.get("/:id/edit", 
+    isLoggedIn, 
+    isOwner, 
+    wrapAsync(listingController.renderEditForm)
+);
 
 module.exports = router;
