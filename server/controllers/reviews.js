db71c977a2af2541edad7df603de0b0a54602c91
Mohammed Tabish Khan <161915192+Tabishkh07@users.noreply.github.com>
2026-02-20 19:35:19 +0530

    Update reviews.js

--- server/controllers/reviews.js
+++ server/controllers/reviews.js
@@ -1,25 +1,47 @@
 const Listing = require("../models/listing.js");
 const Review = require("../models/review.js");
 
+// ================= CREATE REVIEW =================
+module.exports.createReview = async (req, res, next) => {
+    try {
+        const listing = await Listing.findById(req.params.id);
 
-// creating review
-module.exports.createReview = async(req, res)=>{
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-    newReview. author = req.user._id;
-    listing. reviews.push (newReview);
+        if (!listing) {
+            req.flash("error", "Listing not found");
+            return res.redirect("/listings");
+        }
 
-    await newReview.save();
-    await listing. save();
-    req.flash("success", "New Review Created!");
-    res.redirect(`/listings/${listing._id}`);
+        const newReview = new Review(req.body.review);
+        newReview.author = req.user._id;
+
+        listing.reviews.push(newReview);
+
+        await newReview.save();
+        await listing.save();
+
+        req.flash("success", "New Review Created!");
+        return res.redirect(`/listings/${listing._id}`);
+
+    } catch (err) {
+        return next(err);
+    }
 };
 
-// delete review
-module.exports.destroyReview = async(req, res)=>{
-    let {id, reviewId} = req.params;
-    await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
-    await Review.findByIdAndDelete(reviewId);
-    req.flash("success", "Review Deleted!");
-    res.redirect(`/listings/${id}`);
+// ================= DELETE REVIEW =================
+module.exports.destroyReview = async (req, res, next) => {
+    try {
+        const { id, reviewId } = req.params;
+
+        await Listing.findByIdAndUpdate(id, {
+            $pull: { reviews: reviewId },
+        });
+
+        await Review.findByIdAndDelete(reviewId);
+
+        req.flash("success", "Review Deleted!");
+        return res.redirect(`/listings/${id}`);
+
+    } catch (err) {
+        return next(err);
+    }
 };
