89f5ebf09f45712e2ede227cad0c47e7763e505d
Mohammed Tabish Khan <161915192+Tabishkh07@users.noreply.github.com>
2026-03-02 23:32:02 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,14 +1,11 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require("passport-local-mongoose");
-
 const userSchema = new Schema({
     email:{
         type: String,
         required: true
     }  
 });
-
 userSchema.plugin(passportLocalMongoose);     //this will automatically generate username,hashing,salting and hashpassword
-
 module.exports = mongoose.model('User', userSchema);
